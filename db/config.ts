2ef97bfaac6daee23f3fdbe39a65063b14262558
Muhammad Aldan <aldandarmawan@gmail.com>
2025-09-24 23:16:54 +0700

    added astro db, layouting page 7

---
+++ db/config.ts
@@ -0,0 +1,21 @@
+import { defineDb, defineTable, column, NOW } from 'astro:db';
+
+const GuestBook = defineTable({
+  columns: {
+    id: column.number({ primaryKey: true  }),
+    nama: column.text(),
+    hadir: column.boolean(),
+    pesan: column.text(),
+    dibuat_pada: column.date({default: NOW})
+  },
+  indexes: [
+    { on: ["id"], unique: true }
+  ]
+})
+
+// https://astro.build/db/config
+export default defineDb({
+  tables: {
+    GuestBook
+  }
+});
