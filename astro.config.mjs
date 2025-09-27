eeab1363ce893468cbca896bbb04da0e055e7e4f
Muhammad Aldan <aldandarmawan@gmail.com>
2025-09-27 18:57:03 +0700

    installed netlify

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,13 +5,16 @@ import tailwindcss from '@tailwindcss/vite';
 
 import db from '@astrojs/db';
 
+import netlify from '@astrojs/netlify';
+
 // https://astro.build/config
 export default defineConfig({
   vite: {
     plugins: [tailwindcss()]
   },
+
   site: 'https://aldandarmawan.github.io',
   base: '/ngundang',
-
-  integrations: [db()]
+  integrations: [db()],
+  adapter: netlify()
 });
